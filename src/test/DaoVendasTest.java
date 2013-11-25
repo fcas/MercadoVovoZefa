0fdfaa8a7c59ec43d085fffee5b33cbbf06abfd8
fcas <f3l1nux@gmail.com>
2013-11-24 23:25:13 -0300

    Refatoração DAOVendas

--- src/test/DaoVendasTest.java
+++ src/test/DaoVendasTest.java
@@ -24,58 +24,58 @@ public class DaoVendasTest {
 		venda = new Venda();
 	}
 	
-//	@Test
-//	public void testCadastrarVenda(){
-//
-//		daoVendas.apagarTudo();
-//		venda.setDataVenda("hoje");
-//		venda.setQtdVendas(2);
-//		venda.setSubtotal(50);
-//		venda.setVendedorRG("111111111");
-//		
-//		daoVendas.criarVenda(venda);
-//		
-//		assertEquals(1, daoVendas.listarVendas().size());
-//	}
-//	
-//	@Test
-//	public void testeBuscarVenda(){
-//
-//		daoVendas.apagarTudo();
-//		venda.setDataVenda("hoje");
-//		venda.setQtdVendas(2);
-//		venda.setSubtotal(50);
-//		venda.setVendedorRG("111111111");
-//		
-//		daoVendas.criarVenda(venda);
-//		int id = daoVendas.buscarVendaPorNome("hoje");
-//		System.out.println(id);
-//		
-//		assertEquals(venda.getQtdVendas(), daoVendas.buscarVenda(id).getQtdVendas());
-//		assertEquals(venda.getSubtotal(), daoVendas.buscarVenda(id).getSubtotal(), 0);
-//		assertEquals(venda.getVendedorRG(), daoVendas.buscarVenda(id).getVendedorRG());
-//		
-//		assertEquals(id, daoVendas.buscarIdVenda(id));
-//		daoVendas.apagarTudo();
-//		assertEquals(-1, daoVendas.buscarVendaPorNome("hoje"));
-//		assertEquals(-1, daoVendas.buscarIdVenda(id));
-//	}
-//	
-//	@Test
-//	public void testApagarVenda(){
-//		daoVendas.apagarTudo();
-//		venda.setDataVenda("hoje");
-//		venda.setQtdVendas(2);
-//		venda.setSubtotal(50);
-//		venda.setVendedorRG("111111111");
-//		
-//		daoVendas.criarVenda(venda);
-//		int id = daoVendas.buscarVendaPorNome("hoje");
-//		
-//		daoVendas.apagarVenda(id);
-//		
-//		assertEquals(0, daoVendas.listarVendas().size());
-//	}
+	@Test
+	public void testCadastrarVenda(){
+
+		daoVendas.apagarTudo();
+		venda.setDataVenda("hoje");
+		venda.setQtdVendas(2);
+		venda.setSubtotal(50);
+		venda.setVendedorRG("111111111");
+		
+		daoVendas.criarVenda(venda);
+		
+		assertEquals(1, daoVendas.listarVendas().size());
+	}
+	
+	@Test
+	public void testeBuscarVenda(){
+
+		daoVendas.apagarTudo();
+		venda.setDataVenda("hoje");
+		venda.setQtdVendas(2);
+		venda.setSubtotal(50);
+		venda.setVendedorRG("111111111");
+		
+		daoVendas.criarVenda(venda);
+		int id = daoVendas.buscarVendaPorNome("hoje");
+		System.out.println(id);
+		
+		assertEquals(venda.getQtdVendas(), daoVendas.buscarVenda(id).getQtdVendas());
+		assertEquals(venda.getSubtotal(), daoVendas.buscarVenda(id).getSubtotal(), 0);
+		assertEquals(venda.getVendedorRG(), daoVendas.buscarVenda(id).getVendedorRG());
+		
+		assertEquals(id, daoVendas.buscarIdVenda(id));
+		daoVendas.apagarTudo();
+		assertEquals(-1, daoVendas.buscarVendaPorNome("hoje"));
+		assertEquals(-1, daoVendas.buscarIdVenda(id));
+	}
+	
+	@Test
+	public void testApagarVenda(){
+		daoVendas.apagarTudo();
+		venda.setDataVenda("hoje");
+		venda.setQtdVendas(2);
+		venda.setSubtotal(50);
+		venda.setVendedorRG("111111111");
+		
+		daoVendas.criarVenda(venda);
+		int id = daoVendas.buscarVendaPorNome("hoje");
+		
+		daoVendas.apagarVenda(id);
+		
+		assertEquals(0, daoVendas.listarVendas().size());
+	}
 
 	
 }
